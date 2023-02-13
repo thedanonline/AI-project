42527612026a4d0f8c91ba3cd2b7261e2b5fe674
thedanonline <danielyoung1989@hotmail.co.uk>
2023-02-13 18:30:02 +0000

    Add files via upload

--- routes/models/user.js
+++ routes/models/user.js
@@ -34,8 +34,8 @@ const UserSchema = new Schema({
 		get: v => Math.round(v),
 		set: v => Math.round(v),
 	},
-	plan: { type: String, default: "free" }, // entry, pro
-	status: { type: String, default: "active" }, // trialing, active, inactive
+	plan: { type: String, default: "" }, // entry, pro
+	status: { type: String, default: "" }, // trialing, active, inactive
 	trial_end: { type: Date, 
 		default: ((Date.now() / 1000)  + (7 * 24 * 60 * 60)), 
 		set: d => new Date(d * 1000 ) },
